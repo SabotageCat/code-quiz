d7cb01bb344eec7f6d3d14482f156634f4496d75
Robert D Gonzales <robert.gonzo13@yahoo.com>
2022-03-06 16:53:01 -0600

    timer penalty

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,18 +1,21 @@
 var questionsArr = {
 
 };
+var seconds = 100;
 
 var timer = function() {
-    var seconds = 100;
-
     var timer = setInterval(function() {
         document.getElementById("timer").innerHTML='Time: 00:' + seconds; seconds--;
-        if (seconds <= 0) {
+        if (seconds < 0) {
             clearInterval(timer);
             endQuiz();
         }
     }, 1000);
     console.log("Start Timer");
+}
+
+var subtractTime = function() {
+    seconds = seconds - 10;
 }
 
 var startQuiz = function() {
@@ -22,6 +25,9 @@ var startQuiz = function() {
 var endQuiz = function() {
     console.log("End Quiz")
 }
+
+document.getElementById("incorrect-answer").addEventListener("click", subtractTime);
+
 document.getElementById("begin-btn").addEventListener("click", timer);
 
 document.getElementById("begin-btn").addEventListener("click", startQuiz);
