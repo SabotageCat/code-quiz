db6593c1ea40bb7e29fa1f2e08b70f450e844a44
Robert D Gonzales <robert.gonzo13@yahoo.com>
2022-03-07 15:15:05 -0600

    save

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,14 +1,39 @@
-var questionsArr = {
+// questions array
+var questionsArr = [
+    {
+        'question' : 'Test 1',
+        'answer' : 'Correct Answer',
+        'bAnswer' : 'Incorrect Answer',
+        'cAnswer' : 'Incorrect Answer',
+        'dAnswer' : 'Incorrect Answer'
+    },
+    {
+        'question' : 'Test 2',
+        'answer' : 'Correct Answer',
+        'bAnswer' : 'Incorrect Answer',
+        'cAnswer' : 'Incorrect Answer',
+        'dAnswer' : 'Incorrect Answer'
+    },
+    {
+        'question' : 'Test 3',
+        'answer' : 'Correct Answer',
+        'bAnswer' : 'Incorrect Answer',
+        'cAnswer' : 'Incorrect Answer',
+        'dAnswer' : 'Incorrect Answer'
+    }
+];
 
-};
-
+// timer for quiz
 var timer = function() {
+    // 100 seconds for timer countdown
     var seconds = 100;
 
+    //timer countdown and display
     var timer = setInterval(function() {
-        document.getElementById("timer").innerHTML='Time: 00:' + seconds; seconds--;
+        document.getElementById("timer").innerHTML='Time: ' + seconds; seconds--;
         if (seconds <= 0) {
             clearInterval(timer);
+            // if timer runs out, end quiz
             endQuiz();
         }
     }, 1000);
@@ -17,8 +42,24 @@ var timer = function() {
 
 var startQuiz = function() {
     console.log("Start Quiz");
+    // remove text of question and replace it with new text from questionsArr
     var question = document.getElementById("question");
-    question.remove();
+    question.innerText = questions();
+    // remove quiz instructions
+    var instructions = document.getElementById("instructions");
+    instructions.remove();
+    // remove start quiz button
+    var startBtn = document.getElementById("begin-btn");
+    startBtn.remove();
+    // insert questionsArr answers
+
+}
+
+var questions = function() {
+    for (var i = 0; i < questionsArr.length; i++); {
+        var createQuestions = questionsArr[i];
+        createQuestions = document.innerText(createQuestions[i].question);
+    }
 }
 
 var endQuiz = function() {
